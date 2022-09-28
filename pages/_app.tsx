5ddfb06c001320a4614d135ba0bf831e8217dcdb
ShuVeriDA <bashtarov@outlook.com>
2022-09-28 19:59:34 +0300

    refactored MyApp and index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,16 +12,29 @@ import {UserAPI} from "../utils/api/user-api";
 import {setUserData} from "../redux/slices/userSlice";
 import {Component} from "react";
 import { Api } from '../utils/api';
+import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
    return (
-      <MuiThemeProvider theme={theme}>
-         <CssBaseline/>
+      <>
+         <Head>
+            <title>RJournal</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+               href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap"
+               rel="stylesheet"></link>
+         </Head>
+         <MuiThemeProvider theme={theme}>
+            <CssBaseline/>
 
-         <Header/>
-         <Component {...pageProps} />
+            <Header/>
+            <Component {...pageProps} />
 
-      </MuiThemeProvider>
+         </MuiThemeProvider>
+      </>
+
    );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,16 @@
-import Head from 'next/head';
-import {LeftMenu} from '../components/LeftMenu/LeftMenu';
 import {Post} from '../components/Post/Post';
-import {SideComments} from '../components/SideComments/SideComments';
+import {MainLayout} from "../layouts/MainLayout";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>RJournal</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"></link>
-      </Head>
-      {/*<Header />*/}
-      <div className="wrapper">
-        <div className="leftSide">
-          <LeftMenu />
-        </div>
-        <div className="content">
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </div>
-        <div className="rightSide">
-          <SideComments />
-        </div>
-      </div>
-    </div>
-  );
+   return (
+      <MainLayout>
+         <Post/>
+         <Post/>
+         <Post/>
+         <Post/>
+         <Post/>
+         <Post/>
+      </MainLayout>
+
+   );
 }
